e9e646d766f0263bc9e1cffe93b153082c808626
elemouellic <114568852+elemouellic@users.noreply.github.com>
2024-03-12 18:10:28 +0100

    Nitrate Fragment

--- app/src/main/java/fr/vannes/ecoboat/ui/nitrate/NitrateFragment.java
+++ app/src/main/java/fr/vannes/ecoboat/ui/nitrate/NitrateFragment.java
@@ -25,22 +25,41 @@ import java.util.concurrent.TimeUnit;
 import fr.vannes.ecoboat.R;
 import fr.vannes.ecoboat.databinding.FragmentNitrateBinding;
 
+/**
+ * NitrateFragment class
+ */
 public class NitrateFragment extends Fragment {
 
+    // Link the NitrateViewModel to the NitrateFragment's layout
     private FragmentNitrateBinding binding;
 
 
+    /**
+     * This method is called when the fragment is created.
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return Return the View for the fragment's UI, or null.
+     */
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
+        // Create a NitrateViewModel
         NitrateViewModel nitrateViewModel =
                 new ViewModelProvider(this).get(NitrateViewModel.class);
 
+        // Inflate the layout for this fragment
         binding = FragmentNitrateBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         final TableLayout tableLayout = binding.tableNitrateLevel;
 
+        // Observe the LiveData from the NitrateViewModel
         nitrateViewModel.getNitrateDataList().observe(getViewLifecycleOwner(), nitrateDataList -> {
             // Clear the table
             tableLayout.removeAllViews();
@@ -49,6 +68,7 @@ public class NitrateFragment extends Fragment {
             final TableRow headerRow = new TableRow(getContext());
             headerRow.setGravity(Gravity.CENTER);
             String[] headers = {"Heure", "Taux"};
+            // Loop through the headers
             for (String header : headers) {
                 TextView headerCell = new TextView(getContext());
                 headerCell.setText(header);
@@ -63,46 +83,45 @@ public class NitrateFragment extends Fragment {
                 params.setMargins(10, 10, 10, 30); // marge de 10dp
                 headerCell.setLayoutParams(params);
             }
+            // Add the header row to the table
             tableLayout.addView(headerRow);
-            // Add rows to the table
+
+            // Loop through the nitrate data list
             for (String nitrateData : nitrateDataList) {
                 TableRow row = new TableRow(getContext());
                 row.setGravity(Gravity.CENTER);
-
+                // Split the nitrate data
                 String[] parts = nitrateData.split(" : ");
                 for (String part : parts) {
                     TextView cell = new TextView(getContext());
                     cell.setGravity(Gravity.END);
                     cell.setText(part);
-                    cell.setTextSize(16); // taille du texte
-                    cell.setTextColor(Color.BLACK); // couleur du texte
-//                    cell.setBackgroundColor(Color.WHITE); // couleur d'arrière-plan
-                    cell.setPadding(100, 8, 100, 8); // padding
-                    cell.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.cell_border)); // bordure
+                    cell.setTextSize(16);
+                    cell.setTextColor(Color.BLACK);
+                    cell.setPadding(100, 8, 100, 8);
+                    cell.setBackground(ContextCompat.getDrawable(requireContext(), R.drawable.cell_border));
 
                     TableRow.LayoutParams params = new TableRow.LayoutParams();
-                    params.setMargins(10, 10, 10, 10); // marge de 10dp
+                    params.setMargins(10, 10, 10, 10);
                     cell.setLayoutParams(params);
                     row.addView(cell);
                 }
-
+                // Add the row to the table
                 tableLayout.addView(row);
             }
         });
+        // Return the root view
         return root;
     }
 
+    /**
+     * This method is called when the fragment is destroyed.
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
 
-//    @Override
-//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-//        super.onActivityCreated(savedInstanceState);
-//        mViewModel = new ViewModelProvider(this).get(NitrateViewModel.class);
-//        // TODO: Use the ViewModel
-//    }
 
 }
